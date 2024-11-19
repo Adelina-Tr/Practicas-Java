5ed08357a6d021ad5da13b5eb3ccfcd6d4ed10a4
Adelina Trejo <kf8@hotmail.com>
2024-11-18 18:29:14 -0600

    Definición de variables de instancia (atributos)

--- Variables-Java/src/com/practicajava/model/Automovil.java
+++ Variables-Java/src/com/practicajava/model/Automovil.java
@@ -1,5 +1,11 @@
 package com.practicajava.model;
 
 public class Automovil {
-
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int año;
+    private String color;
+    private double precio;
+    private boolean isAsegurado;
 }
