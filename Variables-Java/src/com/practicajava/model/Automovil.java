0bf8c3c27b1f07b1b084ba5e7f2470ff6d9184f9
Adelina Trejo <kf8@hotmail.com>
2024-11-18 18:59:37 -0600

    Generación de métodos equals y toString

--- Variables-Java/src/com/practicajava/model/Automovil.java
+++ Variables-Java/src/com/practicajava/model/Automovil.java
@@ -101,6 +101,33 @@ public class Automovil {
     public void setAsegurado(boolean isAsegurado) {
         this.isAsegurado = isAsegurado;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Automovil other = (Automovil) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil [id=" + id + ", placa=" + placa + "]";
+    }
     
     
 
